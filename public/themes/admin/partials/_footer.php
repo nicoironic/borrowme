731fb0019d14bcc8688a65ffc656d4e2342a912e
Nico Ironic <vaganic@gmail.com>
2013-12-14 17:59:09 +0800

    *First commit - upload all files to git

---
+++ public/themes/admin/partials/_footer.php
@@ -0,0 +1,20 @@
+	<footer class="container-fluid footer">
+		<p class="pull-right">
+			Executed in {elapsed_time} seconds, using {memory_usage}.
+			<br/>
+			Powered by <a href="http://cibonfire.com" target="_blank"><i class="icon-fire">&nbsp;</i>&nbsp;Bonfire</a> <?php echo BONFIRE_VERSION ?>
+		</p>
+	</footer>
+
+	<div id="debug"><!-- Stores the Profiler Results --></div>
+
+    <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo js_path(); ?>jquery-1.7.2.min.js"><\/script>')</script>
+    <script src="<?php echo js_path(); ?>jquery-ui-1.8.13.min.js"></script>
+    <script src="<?php echo js_path(); ?>jquery-ui-timepicker-addon.js"></script>
+
+	<?php echo Assets::js(); ?>
+</body>
+</html>
+
